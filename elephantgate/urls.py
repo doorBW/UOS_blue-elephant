78f052d35ba3be631bfdb7efb6e4f6be89bd9942
TigerCow <doorBW@outlook.com>
2018-02-26 16:07:15 +0900

    six

--- elephantgate/urls.py
+++ elephantgate/urls.py
@@ -1,7 +1,7 @@
 #elephantgate_url
 # -*- coding: utf-8 -*-
 from django.conf.urls import url,include
-from . import views
+import views
 #app_name = 'elephantgate' apps.py 
 #파일에 있는데 어떤 코드에서 여기에 추가하기도 하길래 일단 해놓음
 
